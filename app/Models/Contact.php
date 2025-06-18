c45112f158cb7e3b72f6d2c66bb4d6479c37a578
M3NT0Sz <matheusmendes2005@outlook.com>
2025-06-18 08:38:57 -0300

    Add docblock and comments to Contact model

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -6,10 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * Contact model represents a contact entity with soft delete support.
+ */
 class Contact extends Model
 {
     use HasFactory, SoftDeletes;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
         'name',
         'contact',
