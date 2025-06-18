2c0f661742b28011af0eba1bfbcdb20e7aac4f2e
M3NT0Sz <matheusmendes2005@outlook.com>
2025-06-18 08:39:19 -0300

    Add docblock and comments to ContactFactory

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -5,6 +5,9 @@ namespace Database\Factories;
 use App\Models\Contact;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * Factory for generating fake Contact data for testing and seeding.
+ */
 class ContactFactory extends Factory
 {
     protected $model = Contact::class;
@@ -12,8 +15,11 @@ class ContactFactory extends Factory
     public function definition(): array
     {
         return [
+            // Generate a fake name
             'name' => $this->faker->name(),
+            // Generate a unique 9-digit contact number
             'contact' => $this->faker->unique()->numerify('#########'),
+            // Generate a unique safe email
             'email' => $this->faker->unique()->safeEmail(),
         ];
     }
